feca7cf8edd0c16871942b76e3e426e6adf9d615
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2024-02-16 15:18:16 +0100

    java del

---
+++ src/test/java/com/example/touristguidev2/TouristGuidev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristguidev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuidev2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
